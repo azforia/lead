f38d5cd4e0133438f08e04f23adc53daa0d3742d
azforia <107815306+azforia@users.noreply.github.com>
2022-07-12 15:48:09 +0100

    Delete Leads directory

--- Leads/test.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-trades = sys.argv[1].split()
-links = []
-for i in range(len(trades)):    
-    links.append("https://www.pinpoint7.net/BitcoinERA/?campaigns=Trade" + trades[i] + "&api_key=ROxUP76L7CNfJ8K&landings=BitcoinERA&product=")
-
-print(links)
